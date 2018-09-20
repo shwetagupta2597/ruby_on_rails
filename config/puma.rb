9724827461db5836603866cc8eb0aee670ac2af5
pc-swetha <shweta.gupta@terralogic.com>
2018-09-20 11:29:02 +0530

    new

--- config/puma.rb
+++ config/puma.rb
@@ -9,8 +9,8 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { process.env.PORT || 3000 }
-
+port        ENV.fetch("PORT") {  3000 }   #process.env.PORT ||
+#const PORT = process.env.PORT||'8080';
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
